1c5bac5748b9cd499118441b13d77840ef51ad8c
Xiaoyi Chen <126677643+xxc451@users.noreply.github.com>
2026-02-05 19:45:37 +0100

    Setup project structure

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "node:path";
+
+export default {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(import.meta.dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
